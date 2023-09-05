cc4c9d44cedd12bf923b221d436f4330ab037753
Nathan Vogt <merrick.vogt@gmail.com>
2023-09-05 16:39:53 -0400

    first draft of free-shelf

---
+++ library/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Books from './Book';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Book List</h1>
+      <Books />
+    </div>
+  );
+}
+
+export default App;
